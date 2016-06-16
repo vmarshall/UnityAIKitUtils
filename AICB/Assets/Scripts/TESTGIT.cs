1285208cbe7c31b6c302baa9bde7e777b728448c
Vernon Marshall <vernon.marshall+github@gmail.com>
2016-06-16 08:09:48 -0700

    t2

--- AICB/Assets/Scripts/TESTGIT.cs
+++ AICB/Assets/Scripts/TESTGIT.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TESTGIT : MonoBehaviour {
-
+public class TESTGIT : MonoBehaviour
+{
+	// I AM TEST STRING 111
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 	
 	}
 }
