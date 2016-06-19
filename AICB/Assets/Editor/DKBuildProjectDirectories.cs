4fed0c0658782cbe57fd58fed48587a0efbe0c4e
Vernon Marshall <vernon.marshall+github@gmail.com>
2016-06-19 11:00:09 -0700

    Argh -- don't use this client to add to gitignore

---
+++ AICB/Assets/Editor/DKBuildProjectDirectories.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System.Runtime.InteropServices;
+using System;
+
+/// Store this code as "MakeFolders.cs" in the Assets\Editor directory (create it, if it does not exist)
+/// Creates a number of directories for storage of various content types.
+/// Modify as you see fit.
+/// Directories are created in the Assets dir.
+/// Not tested on a Mac.
+
+ // CHANGE
+ 
+public class DKBuildProjectDirectories : ScriptableObject
+{
+	String topDir = "DK_DEBUG/";
+
+	[MenuItem("DK/BuildProjectDirectories")]
+	static void MenuMakeFolders()
+	{
+		CreateFolders();
+	}
+
+	static void CreateFolders()
+	{
+		string f = Application.dataPath + "/DK_DEBUG/";
+
+
+		Directory.CreateDirectory(f + "Meshes");
+		Directory.CreateDirectory(f + "Fonts");
+		Directory.CreateDirectory(f + "Plugins");
+		Directory.CreateDirectory(f + "Textures");
+		Directory.CreateDirectory(f + "Materials");
+		Directory.CreateDirectory(f + "Physics");
+		Directory.CreateDirectory(f + "Resources");
+		Directory.CreateDirectory(f + "Scenes");
+		Directory.CreateDirectory(f + "Music");
+		Directory.CreateDirectory(f + "Packages");
+		Directory.CreateDirectory(f + "Scripts");
+		Directory.CreateDirectory(f + "Shaders");
+		Directory.CreateDirectory(f + "Sounds");
+
+		Debug.Log("Created directories");
+	}
+}
---
+++ AICB/Assets/Editor/DKDumpScene.cs
@@ -0,0 +1,66 @@
+﻿// SceneDumper.cs
+//
+// History:
+// version 1.0 - December 2010 - Yossarian King
+ 
+using StreamWriter = System.IO.StreamWriter;
+ 
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+using System.Runtime.InteropServices;
+using System;
+
+public static class DKSceneDumper
+{
+	[MenuItem("DK/Debug/DumpScene")]
+	public static void DumpScene()
+	{
+//		string f = Application.dataPath + "Assets/DK_DEBUG/";
+//
+//
+//		Directory.CreateDirectory(f + "DUMPED_SCENES");
+		if ((Selection.gameObjects == null) || (Selection.gameObjects.Length == 0))
+			{
+				Debug.LogError("Please select the object(s) you wish to dump.");
+				return;
+			}
+ 
+		// string filename = @"C:\unity-scene.txt";
+
+		// TODO Clean Up bloody paths
+
+		string filename = "Assets/DK_DEBUG/DUMP/dumped-scene.txt";
+
+		Debug.Log("Dumping scene to " + filename + " ...");
+		using (StreamWriter writer = new StreamWriter(filename, false))
+			{
+				foreach (GameObject gameObject in Selection.gameObjects)
+					{
+						DumpGameObject(gameObject, writer, "");
+					}
+			}
+		Debug.Log("Scene dumped to " + filename);
+	}
+
+	private static void DumpGameObject(GameObject gameObject, StreamWriter writer, string indent)
+	{
+		writer.WriteLine("{0}+{1}", indent, gameObject.name);
+ 
+		foreach (Component component in gameObject.GetComponents<Component>())
+			{
+				DumpComponent(component, writer, indent + "  ");
+			}
+ 
+		foreach (Transform child in gameObject.transform)
+			{
+				DumpGameObject(child.gameObject, writer, indent + "  ");
+			}
+	}
+
+	private static void DumpComponent(Component component, StreamWriter writer, string indent)
+	{
+		writer.WriteLine("{0}{1}", indent, (component == null ? "(null)" : component.GetType().Name));
+	}
+}
